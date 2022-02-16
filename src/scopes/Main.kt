48f96035e73cf815346a02e1e44553334bb26a5c
AnastasiaDro <adrogunova@gmail.com>
2022-02-16 15:27:50 +0300

    - все работает. TODO: нужно раскидать по функциям

--- src/scopes/Main.kt
+++ src/scopes/Main.kt
@@ -2,6 +2,11 @@
 //Date: 10.01.2022 Time 13:05
 
 package scopes
+
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
 //Если все открывающиеся скобки - закрываются в этой строке, необходимо вернуть true
 //Если есть какая то скобка - которая не имеет скобки закрытия, вернуть false.
 //Если есть скобка которая идет не в своем порядке(}{ ]{,
@@ -10,11 +15,35 @@ package scopes
 
 //если нет скобки закрытия - false
 
+
+fun checkScopes(scopeStr: String) : Boolean {
+
+    val scopeMap : HashMap<Char, Char> = hashMapOf('(' to ')', '[' to ']', '{' to '}')
+    //разделить на 2 и выйти если
+    val stack = Stack<Char>()
+    val isEven = scopeStr.length % 2 == 0
+    if (isEven) {
+        stack.push(scopeStr[0])
+        for (index in scopeStr.indices - 0)
+        {
+            when {
+                scopeMap.containsKey(scopeStr[index]) -> stack.push(scopeStr[index])
+                scopeMap.containsValue(scopeStr[index]) && scopeMap.get(stack.peek()) == scopeStr[index] -> stack.pop()
+                else -> throw RuntimeException("Ошибка последовательности скобок!")
+            }
+        }
+    }
+    return (isEven && stack.isEmpty())
+}
+
 fun main() {
-    val scopeStr = "({[([])]})"
-    val arrStr = scopeStr.toCharArray().asList()
+    val scopeStr_valid1 = "({[([])]})"
+    val scopeStr_valid2 = "()(){[[]]()}"
+    val scopeStr_invalid = "((({())))"
+    val scopeStr_exeption1 = "((}{})"
 
-    var myDeq = ArrayDeque(arrStr)
-
-
+    println(checkScopes(scopeStr_valid1))
+    println(checkScopes(scopeStr_valid2))
+    println(checkScopes(scopeStr_invalid))
+    println(checkScopes(scopeStr_exeption1))
 }
